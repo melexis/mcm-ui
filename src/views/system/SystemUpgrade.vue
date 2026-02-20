d630f765f715ce959e0cdf4053ca91fcf427593c
Nico Bollen <nico.bollen@gmail.com>
2026-02-20 10:45:04 +0100

    upgrade mcm813x9 to v0.28.0

--- src/views/system/SystemUpgrade.vue
+++ src/views/system/SystemUpgrade.vue
@@ -23,8 +23,8 @@ const firmwareBaseNames = {
   'Melexis Compact Master LIN': 'mcm-lin',
 };
 const firmwareLatestRev = {
-  'mcm-81339': 'v0.27.0',
-  'mcm-81349': 'v0.27.0',
+  'mcm-81339': 'v0.28.0',
+  'mcm-81349': 'v0.28.0',
   'mcm-lin': 'v0.4.0',
 };
 let firmwareBaseName = '';
